8c1f0752d2e935687284fed858ee2a6521d269a2
Naman Goel <naman34@gmail.com>
2015-07-05 00:43:30 +0200

    new update based on eslint

--- build/index.js
+++ build/index.js
@@ -1,12 +1,55 @@
+/* global atom */
+// import fs from 'fs'
+// import path from 'path'
+// import {sync} from 'resolve'
+// import {exec} from 'child_process'
+// import {CompositeDisposable} from 'atom'
+// import {allowUnsafeNewFunction} from 'loophole'
+
 'use strict';
 
-module.exports = { grammarScopes: ['source.js', 'source.jsx', 'source.es6'],
-  scope: 'file',
-  lintOnFly: true,
-  lint: function lint(textEditor) {
-    console.debug(textEditor);
-    return new Promise(function (res) {
-      res({ type: 'Error', text: 'Just Testing', range: [[0, 0], [0, 1]] });
-    });
+var linterPackage = atom.packages.getLoadedPackage('linter');
+if (!linterPackage) {
+  return atom.notifications.addError('Linter should be installed first, `apm install linter`', { dismissable: true });
+}
+
+// const linterPath = linterPackage.path
+// const findFile = require(`${linterPath}/lib/util`)
+
+module.exports = { config: { testRuleOne: { type: 'string',
+      'default': ''
+    },
+    testRuleTwo: { type: 'boolean',
+      'default': false
+    }
+  },
+  activate: function activate() {
+    console.log('activating linter-flow-plus');
+  },
+  deactivate: function deactivate() {
+    console.log('deactivating linter-flow-plus');
+  },
+  provideLinter: function provideLinter() {
+    var provider = { grammarScopes: ['source.js', 'source.js.jsx', 'source.babel', 'source.js-semantic', 'source.es6'],
+      scope: 'file',
+      lintOnFly: true,
+      lint: function lint(TextEditor) {
+        var filePath = TextEditor.getPath();
+        console.log('TextEditor: ', TextEditor);
+
+        // no errors: return []
+
+        return [{ type: 'warning',
+          html: '<span class="badge badge-flexible">21</span> Test Error',
+          filePath: filePath,
+          range: [[0, 0], [0, 1]]
+        }];
+      }
+    };
+
+    return provider;
   }
 };
+// eslint-disable-line
+// getting custom value
+// const customValueOne: string = atom.config.get('linter-flow-plus.testRuleOne')
--- src/index.js
+++ src/index.js
@@ -1,11 +1,61 @@
+/* global atom */
+// import fs from 'fs'
+// import path from 'path'
+// import {sync} from 'resolve'
+// import {exec} from 'child_process'
+// import {CompositeDisposable} from 'atom'
+// import {allowUnsafeNewFunction} from 'loophole'
+
+const linterPackage = atom.packages.getLoadedPackage('linter')
+if(!linterPackage){
+  atom.notifications.addError('Linter should be installed first, `apm install linter`', {dismissable: true}) // eslint-disable-line
+}
+
+// const linterPath = linterPackage.path
+// const findFile = require(`${linterPath}/lib/util`)
+
 module.exports =
-  { grammarScopes: ['source.js', 'source.jsx', 'source.es6']
-  , scope: 'file'
-  , lintOnFly: true
-  , lint(textEditor) {
-      console.debug(textEditor)
-      return new Promise((res) => {
-        res({type: 'Error', text: 'Just Testing', range: [[0, 0], [0, 1]]})
-      })
+  { config:
+      { testRuleOne:
+          { type: 'string'
+          , default: ''
+          }
+      , testRuleTwo:
+          { type: 'boolean'
+          , default: false
+          }
+      }
+  , activate(){
+      console.log('activating linter-flow-plus')
+
+      // getting custom value
+      // const customValueOne: string = atom.config.get('linter-flow-plus.testRuleOne')
+    }
+  , deactivate(){
+      console.log('deactivating linter-flow-plus')
+    }
+  , provideLinter(){
+      const provider =
+        { grammarScopes: ['source.js', 'source.js.jsx', 'source.babel', 'source.js-semantic', 'source.es6']
+        , scope: 'file'
+        , lintOnFly: true
+        , lint(TextEditor){
+            const filePath = TextEditor.getPath()
+            console.log('TextEditor: ', TextEditor)
+
+            // no errors: return []
+
+            return [
+              { type: 'warning'
+              , html: '<span class="badge badge-flexible">21</span> Test Error'
+              , filePath: filePath
+              , range: [[0, 0], [0, 1]]
+              }
+            ]
+
+          }
+        }
+
+      return provider
     }
   }
